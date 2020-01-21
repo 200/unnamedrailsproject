b65524440a990bdff857ed66cf788969613cb3ea
Nikodem Macuk <niko.macuk@gmail.com>
2020-01-21 17:33:12 +0100

    added the possibility to create new users via admin panel with unique and valid email adress

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    # Validation for unique and valid email addres
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+    validates :email, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+end
