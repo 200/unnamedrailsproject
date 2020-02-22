937657cf8ede96d855d9012a2a77340517293b9f
Nikodem <niko.macuk@gmail.com>
2020-02-22 16:03:30 +0100

    Added shoulda-matchers in order to shorten rspec tests code and updated gems for travis-ci

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ApplicationRecord
-    validates :email, uniqueness: { case_sensitive: false }, format: { with: Devise.email_regexp }
+  validates_presence_of :email, uniqueness: { case_sensitive: false }, format: { with: Devise.email_regexp }
 end
