8b21e75a3e6b58d369360a318ab0efcd1524992c
Nikodem <niko.macuk@gmail.com>
2020-03-28 10:05:28 +0100

    refactor(user): refractor user model to be compatible with devise
    fix(user): changed routes for devisse/active admin
    
    fix(user): fixed form approval

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register User do
   permit_params :email
+  form do |f|
+    f.inputs do
+      f.input :email, uniqueness: { case_sensitive: false }, format: { with: Devise.email_regexp }
+    end
+    f.actions
+  end
+
+  after_create(&:send_reset_password_instructions)
+
+  def password_required?
+    new_record? ? false : super
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
 class User < ApplicationRecord
-  validates_presence_of :email, uniqueness: { case_sensitive: false }, format: { with: Devise.email_regexp }
-
-  def self.serialize_into_session(record)
-    [record.to_key.map(&:to_s), record.authenticatable_salt]
-  end
-
-  def self.serialize_into_cookie(record)
-    [record.to_key.map(&:to_s), record.rememberable_value]
-  end
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
