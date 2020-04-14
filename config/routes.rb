7dfcad1a3328969cf489ff98228f474f67446622
Nikodem <niko.macuk@gmail.com>
2020-04-14 16:49:11 +0200

    improvement(mailer): added letter_opener gem for dev env

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # get 'main/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root to: "main#index"
 end
