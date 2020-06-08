5bed78d663088f692da076eb17869a6ae39a8172
Piotr Misiurek <piotr@ragnarson.com>
2020-06-08 20:58:48 +0700

    Use devise test helpers for sign_in in specs

--- spec/features/admin/users_management_spec.rb
+++ spec/features/admin/users_management_spec.rb
@@ -1,17 +1,12 @@
 require "rails_helper"
 
 feature "users management" do
-  let(:password) { "password" }
   let(:user_email) { "foobar@example.com" }
-  let!(:admin_user) { create(:admin_user, password: password, password_confirmation: password) }
+  let!(:admin_user) { create(:admin_user) }
 
   background do
+    sign_in(admin_user, scope: :admin_user)
     visit "/admin"
-    within("form.admin_user") do
-      fill_in "admin_user_email", with: admin_user.email
-      fill_in "admin_user_password", with: password
-      click_button("Login")
-    end
   end
 
   scenario "Create user" do
