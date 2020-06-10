37b50cb1e71f8650cd72d53f6e4431bdd162be2e
Piotr Misiurek <piotr@ragnarson.com>
2020-06-10 14:25:41 +0700

    Sentry dsn is required

--- config/initializers/raven_sentry.rb
+++ config/initializers/raven_sentry.rb
@@ -1,6 +1,6 @@
 if %w(staging production).include?(Rails.env)
   Raven.configure do |config|
-    config.dsn = ENV["SENTRY_DSN"]
+    config.dsn = ENV.fetch("SENTRY_DSN")
     config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   end
 end
